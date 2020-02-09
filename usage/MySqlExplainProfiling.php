d579803bad0dd6740e6f304c7f7044a7e89b3aff
党留辉 <liuhuidang@sf-express.com>
2020-02-09 16:48:29 +0800

    增加示例代码

---
+++ usage/MySqlExplainProfiling.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Desc:
+ * User: baagee
+ * Date: 2019/10/26
+ * Time: 15:49
+ */
+
+namespace Sf;
+
+use Sftcwl\Log\Log;
+use Sftcwl\Orm\Connection;
+use Sftcwl\Orm\DB;
+
+/**
+ * Class MySqlExplainProfiling
+ * @package Sf
+ */
+class MySqlExplainProfiling extends SqlExplainProfilingAbstract
+{
+    /**
+     * @return mixed|Connection
+     */
+    protected function getMySqlConnection()
+    {
+        return Connection::getConnection('srm', true);
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isOpenProfiling(): bool
+    {
+        $res = self::$connection->query(self::SELECT_PROFILING_SQL);
+        if (isset($res[0]['p_v'])) {
+            if ($res[0]['p_v'] == '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @return mixed|void
+     */
+    protected function openProfiling()
+    {
+        self::$connection->query(self::OPEN_PROFILING_SQL);
+    }
+
+    /**
+     * @return mixed|void
+     */
+    protected function closeProfiling()
+    {
+        self::$connection->query(self::CLOSE_PROFILING_SQL);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getProfiles(): array
+    {
+        $ret = self::$connection->query(self::SHOW_PROFILES_SQL);
+        return (array)$ret;
+    }
+
+    /**
+     * @param int $queryId
+     * @return array
+     */
+    protected function getProfileDetail(int $queryId): array
+    {
+        $ret = self::$connection->query(self::SHOW_PROFILE_QUERY_SQL . strval($queryId));
+        return (array)$ret;
+    }
+
+    /**
+     * @param string $fullSql
+     * @return array
+     */
+    protected function getSqlExplain(string $fullSql): array
+    {
+        $ret = self::$connection->query(self::EXPLAIN_SQL . $fullSql);
+        return (array)$ret;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getProjectName(): string
+    {
+        return strval(array_pop(explode(DIRECTORY_SEPARATOR, ROOT_PATH)));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getModuleName(): string
+    {
+        return strval(AppEnv::getCurrApp());
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTraceId(): string
+    {
+        return strval(Log::genLogID());
+    }
+
+    /**
+     * @param array $profile
+     * @return string
+     */
+    protected function getFullSql(array $profile): string
+    {
+        $allSqls = DB::getSqls();
+        return trim($allSqls[$profile['Query_ID'] + 1] ?? $profile['Query']);//获取真实的sql 没截断的
+    }
+
+    /**
+     * @param $requestData
+     * @return array
+     */
+    protected function beforeRequest($requestData): array
+    {
+        $url = 'http://127.0.0.1:8580/api/receiver';
+        return [
+            'url'     => $url,
+            'data'    => $requestData,
+            'method'  => 'POST',
+            'headers' => ['Content-Type: application/json'],
+            'is_json' => true
+        ];
+    }
+}
--- usage/README.md
+++ usage/README.md
@@ -1,14 +1,4 @@
 # 简单示例怎么获取，提交profiling数据到工具去分析
 
-在项目的入口index.php 开头引入MySqlProfiling类（或者通过composer加载）
+可以继承`SqlExplainProfilingAbstract`抽象类重写，可参考MySqlExplainProfiling.php
 
-然后修改一下MySqlProfiling类里面的TODO项，
-
-index.php里面加入以下代码，注意命名空间
-
-```php
-$profiling=new MySqlProfiling();
-$profiling->start();
-```
-
-其他情况类似，都是在程序执行刚开始执行这段代码，这样才能完整的监控到整个流程的sql信息
