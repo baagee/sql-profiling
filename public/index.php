312601ebf6b89b273a23b1857f61f8d7ec594a2b
党留辉 <liuhuidang@sf-express.com>
2020-06-16 23:07:08 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
 <?php
-/**
- * Desc:
- * User: baagee
- * Date: 2019/10/26
- * Time: 21:02
- */
 
 require_once implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'vendor', 'autoload.php']);
 
-(new \BaAGee\NkNkn\App())->run();
+(new \BaAGee\NkNkn\HttpApp())->run();
+
